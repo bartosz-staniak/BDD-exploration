c1591d2b8cff8319455e30361ee53a68628d828c
bs <bartosz.staniak@gmail.com>
2022-04-23 15:28:58 +0200

    started a C# project inside the folder

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
