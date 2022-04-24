be973befba20fa93135aab0068bc8327814e5c77
bs <bartosz.staniak@gmail.com>
2022-04-24 04:16:13 +0200

    added a class

---
+++ TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BDD
+{
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
